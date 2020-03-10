35967351941ca86715cc910b3fbfc2214d4da402
Matthew Cheney <matt1cheney@gmail.com>
2020-03-09 21:33:43 -0400

    almost final product

--- server.js
+++ server.js
@@ -1,32 +1,81 @@
 const inquirer = require("inquirer");
-// function definitions
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol() {
-  let symbols = "!@#$%^&*()_+{}[]<>,.";
-  return symbols[Math.floor(Math.random() * 20)];
-}
-const validLength= async (input)=> {
-  if(input < 8 || input > 128 ||typeof input !== 'number' ) {
-
-    throw " invalid length!";
+const validLength= async (input) => {
+  let notValid= isNaN(input);
+  if((input < 8 )|| (input > 128)){
+    
+   throw "invalid entry";
     
     
     
   }
+  else if(notValid){
+    throw "please enter a number"
+  }
   
+  return true;
   
-return true;
+}
+// declare generator class
+class Generator {
+  _password='';
+  // function that executes once when a new instance is made, fired automatically.
+  constructor(choicesArr) {
 
+   this.calcPassword(choicesArr)
+  }
+  //getter
+  get password() {
+    return this._password;
+  }
+// function definitions
+getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+ getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+ getRandomSymbol() {
+  let symbols = "!@#$%^&*()_+{}[]<>,.";
+  return symbols[Math.floor(Math.random() * 20)];
+}
+  calcPassword(choices) {
+  
+    let randomFunction = {
+      uppercase: this.getRandomUpper,
+      lowercase: this.getRandomLower,
+      numbers: this.getRandomNumber,
+      symbols: this.getRandomSymbol
+    };
+    console.log(choices,'choicesarr')
+    let writePassword = "";
+    // let typesArr = [
+    //   { uppercase },
+    //   { lowercase },
+    //   { number },
+    //   { symbols }
+    // ];
+    // filter(
+    //   item => Object.values(item)[0]);
+    
+    for (let i = 0; i < characterAmount; i++) {
+      let randomIndex= Math.floor(Math.random() * choices.length)
+
+       const randomChosen =  choices[randomIndex]
+      console.log(randomChosen,'randomChoice')
+      writePassword += randomFunction[randomChosen]();
+    
+    }
+    this._password= writePassword;
+// move the table into a seperate funtion in class req. choicesArr parameter. call it in constructor after calcPassword.
+    console.table(["chosen character length",characterAmount,choices,"your password",this._password],);
+
+  }
 }
 
 inquirer
@@ -35,19 +84,20 @@ inquirer
       name: "characterAmount",
       message:
       "please specify how many characters in your password.(must be between 8 and 128)",
-      type:"input",
+      type:"number",
+      
       validate: validLength,
       
-      validate: function( value ) {
-        if(value >7 || value <129){
-          var valid = !isNaN(parseFloat(value));
-          return valid;
-        }
-        else{
-          throw " invalid";
-        }
-          },
-          filter: Number
+      // validate: function( value ) {
+      //   if(value >7 || value <129){
+      //     var valid = !isNaN(parseFloat(value));
+      //     return valid;
+      //   }
+      //   else{
+      //     throw " invalid";
+      //   }
+      //     },
+      //     filter: Number
     
       
       
@@ -67,57 +117,12 @@ inquirer
     number = response.choices[2];
     symbols = response.choices[3];
     characterAmount = response.characterAmount;
+    console.log('choices', response.choices)
+    
 
-    // declare generator class
-    class generator {
-      constructor(uppercase, lowercase, number, symbols) {
-        this.uppercase = uppercase;
-        this.lowercase = lowercase;
-        this.number = number;
-        this.symbols = symbols;
-      }
-      //getter
-      get password() {
-        return this.calcPassword();
-      }
-
-      calcPassword() {
-        let randomFunction = {
-          uppercase: getRandomUpper,
-          lowercase: getRandomLower,
-          number: getRandomNumber,
-          symbols: getRandomSymbol
-        };
-        
-        let writePassword = "";
-        let typesArr = [
-          { uppercase },
-          { lowercase },
-          { number },
-          { symbols }
-        ];
-
-        for (let i = 0; i < characterAmount; i++) {
-
-          let funcName = Object.keys(
-            typesArr[Math.floor(Math.random() * typesArr.length)]
-          )[0];
-
-          writePassword += randomFunction[funcName]();
-        
-        }
-        let finalPassword = writePassword.slice(0, characterAmount);
-        console.log(finalPassword);
-
-      }
-    }
-
-    const generatedPassword = new generator(
-      uppercase,
-      lowercase,
-      number,
-      symbols
+    const generatedPassword = new Generator(
+      response.choices
     );
 
-    generatedPassword.password
+    generatedPassword.password;
   });
